79f6b019c5f162d4b3bcb79a6bdebaa245a17db2
Leshan Sanjeewa <143873948+leshan99@users.noreply.github.com>
2024-03-01 09:54:41 +0530

    create navigation bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
 
 export default function Navbar() {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="navlogo">
+        <img src={logo} alt=''/>
+        <p>LIQUOR SHOP</p>
+      </div>
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>Gin</li>
+        <li>Rum</li>
+        <li>Vodka</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt=""/>
+      </div>
+    </div>
   )
 }
