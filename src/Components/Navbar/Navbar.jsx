77776a28f540bc8b4ee157a3ea376f2041a162cb
Leshan Sanjeewa <143873948+leshan99@users.noreply.github.com>
2024-03-01 11:02:48 +0530

    added css to navgation bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 
 export default function Navbar() {
+
+  const [menu,setMenu] = useState("shop");
+
   return (
-    <div className='navbar'>
-      <div className="navlogo">
+    <div className='nav-bar'>
+      <div className="nav-logo">
         <img src={logo} alt=''/>
         <p>LIQUOR SHOP</p>
       </div>
       <ul className="nav-menu">
-        <li>Home</li>
-        <li>Gin</li>
-        <li>Rum</li>
-        <li>Vodka</li>
+        <li onClick={()=>{setMenu("home")}}> Home{menu==="shop"?<hr/>:<></>} </li>
+        <li onClick={()=>{setMenu("gin")}}> Gin{menu==="gin"?<hr/>:<></>} </li>
+        <li onClick={()=>{setMenu("rum")}}> Rum{menu==="rum"?<hr/>:<></>} </li>
+        <li onClick={()=>{setMenu("vodka")}}> Vodka{menu==="vodka"?<hr/>:<></>} </li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
         <img src={cart_icon} alt=""/>
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
   )
