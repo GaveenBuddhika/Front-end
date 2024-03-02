3728f17f7bc75249c3e40d67363116b2d6ce4f7d
Oshada Dilshan <dilshan-se20012@stu.kln.ac.lk>
2024-03-02 15:33:25 +0530

    CSS part 1 done

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -20,6 +20,7 @@ export const ContactUs = () => {
         .then(
           () => {
             console.log('SUCCESS!');
+            e.target.reset();
           },
           (error) => {
             console.log('FAILED...', error.text);
@@ -28,20 +29,27 @@ export const ContactUs = () => {
     };
 
   return (
-    <div className='contactus'>ContactUs pko
     
+      <div className='contactus'>
+
+        <div id='header1'>
+          <h1>Contact Us</h1>
+        </div>
+
+
+          <form id='formContact' ref={form} onSubmit={sendEmail}>
+            <label className='label1'>Name</label>
+            <input className='input1' type="text" name="user_name" />
+            <label className='label1'>Email</label>
+            <input className='input1' type="email" name="user_email" />
+            <label className='label1'>Message</label>
+            <textarea id='textarea1' name="message" />
+            <input id='button1' type="submit" value="Send" />
+          </form>
+
+      </div>
+  
     
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
-      </form>
-    
-    </div>
 
     
   )
